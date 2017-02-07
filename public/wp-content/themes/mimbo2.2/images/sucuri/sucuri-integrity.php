95b7b67c8bd41eded55706a270d235f3514a4672
JR <liveaverage@gmail.com>
2017-02-06 22:20:13 -0500

    Updating site

--- public/wp-content/themes/mimbo2.2/images/sucuri/sucuri-integrity.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=integrity;
-// created_on=1450237691;
-// updated_on=1450237691;
-exit(0);
-?>
--- public/wp-content/themes/mimbo2.2/images/sucuri/sucuri-trustip.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=trustip;
-// created_on=1450237682;
-// updated_on=1450237682;
-exit(0);
-?>
